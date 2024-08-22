6645287d4bf62c5ac6c40a2336a9aebdd310d8ad
Ivan <korablev2002@yandex.ru>
2024-08-22 15:05:03 +0400

    added additional types of buttons

--- app/src/main/java/com/glitchcode/flowery/core/presentation/components/Buttons.kt
+++ app/src/main/java/com/glitchcode/flowery/core/presentation/components/Buttons.kt
@@ -1,5 +1,6 @@
 package com.glitchcode.flowery.core.presentation.components
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
@@ -8,15 +9,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun FloweryFilledButton(
+fun FloweryButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit,
     isEnabled: Boolean = true,
     colors: ButtonColors = ButtonDefaults.buttonColors(),
     content: @Composable (RowScope.() -> Unit)
-
 ) {
     Button(
         modifier = modifier,
@@ -29,11 +31,31 @@ fun FloweryFilledButton(
 }
 
 @Composable
+fun FloweryButtonWithIcon(
+    modifier: Modifier = Modifier,
+    icon: ImageVector,
+    onClick: () -> Unit,
+    isEnabled: Boolean = true,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    content: @Composable RowScope.() -> Unit
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        shape = MaterialTheme.shapes.small,
+        enabled = isEnabled,
+        colors = colors,
+        contentPadding = ButtonDefaults.ButtonWithIconContentPadding,
+        content = content
+    )
+}
+
+@Composable
 fun FloweryTextButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit,
-    isEnabled: Boolean,
-    colors: ButtonColors,
+    isEnabled: Boolean = true,
+    colors: ButtonColors = ButtonDefaults.textButtonColors(),
     content: @Composable RowScope.() -> Unit
 
 ) {
@@ -43,6 +65,44 @@ fun FloweryTextButton(
         shape = MaterialTheme.shapes.small,
         enabled = isEnabled,
         colors = colors,
+        content = content
+    )
+}
+
+@Composable
+fun FloweryTextButtonWithIcon(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    isEnabled: Boolean = true,
+    colors: ButtonColors = ButtonDefaults.textButtonColors(),
+    content: @Composable RowScope.() -> Unit
+) {
+    TextButton(
+        modifier = modifier,
+        onClick = onClick,
+        shape = MaterialTheme.shapes.small,
+        enabled = isEnabled,
+        colors = colors,
+        contentPadding = ButtonDefaults.TextButtonWithIconContentPadding,
+        content = content
+    )
+}
+
+@Composable
+fun FloweryTextButtonCompact(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    isEnabled: Boolean = true,
+    colors: ButtonColors = ButtonDefaults.textButtonColors(),
+    content: @Composable RowScope.() -> Unit
+) {
+    TextButton(
+        modifier = modifier,
+        onClick = onClick,
+        shape = MaterialTheme.shapes.small,
+        enabled = isEnabled,
+        colors = colors,
+        contentPadding = PaddingValues(horizontal = 4.dp),
         content = content
     )
 }
--- app/src/main/java/com/glitchcode/flowery/login/presentation/LoginScreen.kt
+++ app/src/main/java/com/glitchcode/flowery/login/presentation/LoginScreen.kt
@@ -1,15 +1,28 @@
 package com.glitchcode.flowery.login.presentation
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.glitchcode.flowery.R
+import com.glitchcode.flowery.core.presentation.components.FloweryTextButton
+import com.glitchcode.flowery.core.presentation.components.FloweryTextButtonCompact
+import com.glitchcode.flowery.core.presentation.components.FloweryTextField
 import com.glitchcode.flowery.core.presentation.components.notification.SwipeableNotification
 
 @Composable
@@ -22,7 +35,7 @@ fun LoginScreen(
         modifier = Modifier
             .fillMaxSize()
     ) {
-        Button(
+        FloweryTextButtonCompact(
             modifier = Modifier
                 .align(Alignment.Center),
             onClick = { 
@@ -34,6 +47,18 @@ fun LoginScreen(
         ) {
             Text(text = "show notification")
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        var textValue by remember {
+            mutableStateOf("")
+        }
+        FloweryTextField(
+            value = textValue,
+            onValueChanges = {
+                textValue = it
+            },
+            titleText = "Description",
+            labelText = "Enter description..."
+        )
     }
 
     SwipeableNotification(notificationState = notificationState)
