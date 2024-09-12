f8088d3cfbeebf2bcacb33c2c491f54f4f5def11
Ivan <korablev2002@yandex.ru>
2024-09-12 22:43:13 +0500

    Added SwipeableNotification and loading indicator for logout button

--- app/src/main/java/com/glitchcode/flowery/home/presentation/MainScreen.kt
+++ app/src/main/java/com/glitchcode/flowery/home/presentation/MainScreen.kt
@@ -1,15 +1,29 @@
 package com.glitchcode.flowery.home.presentation
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.glitchcode.flowery.core.presentation.components.FloweryButton
+import com.glitchcode.flowery.core.presentation.components.notification.SwipeableNotification
 import kotlinx.coroutines.flow.collectLatest
 
 @Composable
@@ -18,7 +32,8 @@ fun HomeScreen(
     onLoginDataOutdated: () -> Unit,
     viewModel: MainScreenViewModel = hiltViewModel()
 ) {
-
+    val notificationState = viewModel.notificationState
+    
     LaunchedEffect(null) {
         viewModel.isLoggedIn.collectLatest { isLoggedIn ->
             if (!isLoggedIn) onLoginDataOutdated.invoke()
@@ -30,12 +45,34 @@ fun HomeScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        val isLoggingOut = viewModel.isLoggingOut.collectAsState()
+
         FloweryButton(
             onClick = {
                 viewModel.logout(onNavigateToLoginScreen)
             }
         ) {
             Text(text = "Logout")
+            AnimatedVisibility(
+                visible = isLoggingOut.value,
+                enter = expandHorizontally(
+                    animationSpec = spring(stiffness = Spring.StiffnessMediumLow)
+                ),
+                exit = shrinkHorizontally(
+                    animationSpec = spring(stiffness = Spring.StiffnessMediumLow)
+                )
+            ) {
+                Row {
+                    Spacer(modifier = Modifier.width(12.dp))
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(16.dp),
+                        color = MaterialTheme.colorScheme.onPrimary,
+                        strokeWidth = 3.dp
+                    )
+                }
+            }
         }
     }
+    
+    SwipeableNotification(notificationState = notificationState)
 }
--- app/src/main/java/com/glitchcode/flowery/home/presentation/MainScreenViewModel.kt
+++ app/src/main/java/com/glitchcode/flowery/home/presentation/MainScreenViewModel.kt
@@ -2,12 +2,17 @@ package com.glitchcode.flowery.home.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.glitchcode.flowery.R
 import com.glitchcode.flowery.core.domain.repository.LocalAuthDataRepository
 import com.glitchcode.flowery.core.domain.utils.Resource
+import com.glitchcode.flowery.core.presentation.components.notification.SwipeableNotificationState
 import com.glitchcode.flowery.login.domain.usecases.AuthUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,9 +24,13 @@ class MainScreenViewModel @Inject constructor(
     private val authUseCase: AuthUseCase
 ): ViewModel() {
 
+    val notificationState = SwipeableNotificationState()
     // TODO: when NotAuthorizedScreen will be a component, replace this with a bool state
     private val _isLoggedIn = Channel<Boolean>()
     val isLoggedIn = _isLoggedIn.receiveAsFlow()
+
+    private val _isLoggingOut = MutableStateFlow(false)
+    val isLoggingOut = _isLoggingOut.asStateFlow()
 
     init {
         updateAuthInfo()
@@ -39,9 +48,19 @@ class MainScreenViewModel @Inject constructor(
     fun logout(
         onLogoutComplete: () -> Unit
     ) {
+        if (isLoggingOut.value) return
         viewModelScope.launch {
+            _isLoggingOut.update { true }
             val result = authUseCase.logout()
-            if (result is Resource.Success) onLogoutComplete.invoke()
+            if (result is Resource.Success) {
+                onLogoutComplete.invoke()
+            } else {
+                notificationState.showNotification(
+                    titleRes = R.string.not_authorized_screen_notification_title,
+                    textRes = R.string.not_authorized_screen_notification_text
+                )
+            }
+            _isLoggingOut.update { false }
         }
     }
 
